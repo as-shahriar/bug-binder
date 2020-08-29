075614841d712e8b9296c26dbc796454833f6628
asifo1 <a.shahriar1999@gmail.com>
2020-08-30 03:22:39 +0600

    Fixed user profile card margin

--- bugbinder/core/urls.py
+++ bugbinder/core/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from core import views
 urlpatterns = [
-    path('dashboard', views.dashboard),
+    path('dashboard', views.dashboard, name="dashboard"),
     path('u', views.userView),
 
 ]
