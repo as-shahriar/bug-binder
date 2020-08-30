b243c6e9dd438e0168f908a56379a3d1bb3f7815
asifo1 <a.shahriar1999@gmail.com>
2020-08-30 16:20:48 +0600

    Added Password change modal

--- bugbinder/static/js/profile.js
+++ bugbinder/static/js/profile.js
@@ -1,18 +1,25 @@
-edit = document.getElementById("edit-btn");
-save = document.getElementById("save-btn");
-edit_section = document.getElementById("edit-section");
-view_section = document.getElementById("view-section");
+document.addEventListener("DOMContentLoaded", function () {
+  edit = document.getElementById("edit-btn");
+  save = document.getElementById("save-btn");
+  edit_section = document.getElementById("edit-section");
+  view_section = document.getElementById("view-section");
+  changep = document.getElementById("passwordChange-triger");
+  edit.addEventListener("click", () => {
+    view_section.style.display = "none";
+    edit_section.style.display = "flex";
+    edit.style.display = "none";
+    save.style.display = "block";
+    changep.style.display = "block";
+  });
 
-edit.addEventListener("click", () => {
-  view_section.style.display = "none";
-  edit_section.style.display = "flex";
-  edit.style.display = "none";
-  save.style.display = "block";
-});
+  save.addEventListener("click", () => {
+    view_section.style.display = "flex";
+    edit_section.style.display = "none";
+    edit.style.display = "block";
+    save.style.display = "none";
+    changep.style.display = "none";
+  });
 
-save.addEventListener("click", () => {
-  view_section.style.display = "flex";
-  edit_section.style.display = "none";
-  edit.style.display = "block";
-  save.style.display = "none";
+  var elems = document.querySelectorAll(".modal");
+  var instances = M.Modal.init(elems);
 });
